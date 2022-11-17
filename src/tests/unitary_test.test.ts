188b07b6ec8f4bb2a7f31079d979986946b192e6
José Alberto Gómez García <modej@correo.ugr.es>
2022-11-17 09:17:07 +0100

    [#17] :construction: Tests skeleton

---
+++ src/tests/unitary_test.test.ts
@@ -0,0 +1,78 @@
+import { ProductError } from "../errors/product_error";
+import { handler } from "../handler";
+import { Product } from "../models/product";
+import { ProductType } from "../models/product_type";
+
+let aProduct: Product
+
+beforeAll(() => {
+})
+
+describe('Tests unitarios', () => {
+
+    it('Debería probar la creación de productos', () => {
+        expect(aProduct).toBeDefined()
+
+    })
+
+    it('Debería probar los getters de los productos', () => {
+    
+    
+    })
+
+
+    it('Debería probar la creación y añadido de facturas', () => {
+
+
+    })
+
+    it('Debería obtener una factura', () => {
+
+    })
+
+    it('Debería añadir un producto a una factura', () => {
+
+    })
+
+    it('Debería obtener productos de una factura', () => {
+
+    })
+
+    it('Debería poder actualizar productos de una factura', () => {
+
+    })
+
+    it('Debería calcular el total de la factura', () => {
+
+    })
+
+    it('Debería eliminar un producto de la factura', () => {
+
+    })
+
+    it('Debería eliminar una factura', () => {
+            
+    })
+
+    it('Debería añadir productos al almacén', () => {
+
+    })
+
+    
+    it('Debería obtener productos del almacén', () => {
+
+    })
+
+    
+    it('Debería eliminar productos del almacén', () => {
+
+    })
+
+    
+    it('Debería asegurar cantidades positivas de los productos del almacén', () => {
+
+    })
+
+
+
+})
