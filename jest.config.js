b063e5c1929a3ee5a52523e761ef4c273a2a88f9
José Alberto Gómez García <modej@correo.ugr.es>
2022-12-28 11:11:45 +0100

    Revert

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,3 @@ module.exports = {
     testPathIgnorePatterns: ['/node_modules/', '/dist/'],
     coverageDirectory: 'docs/coverage',
 };
-
-// More options can be specified, I think this is enough for now.
-// Wait, this triggers the CI but src doesn't?
