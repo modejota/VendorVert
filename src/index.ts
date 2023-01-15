322818dc6703b097fd3363f9ef9a114b9edd6b55
José Alberto Gómez García <modej@correo.ugr.es>
2023-01-15 12:49:16 +0100

    [#20] Manage server to connect Mongo

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,29 @@
-import app from './app';
-import { configuration as cfg} from './config';
-import { logger } from './utils/logger';
+import { configuration as cfg } from './config';
+import mongoose from 'mongoose'
+import {logger} from './utils/logger'
+import app from './app'
 
-app.listen( {port: cfg.fastify_port}, (err, address) => {
-    if (err) {
-        console.error(err);
-        logger.error(err);
-        process.exit(1);
-    }
-    console.log(`Server listening at ${address}`);
-    logger.info(`Server listening at ${address}`);
+const start = async () => 
+    app.listen( {port: cfg.fastify_port}, (err, address) => {
+        if (err) {
+            console.error(err);
+            logger.error(err);
+            process.exit(1);
+        }
+        console.log(`Server listening at ${address}`);
+        logger.info(`Server listening at ${address}`);
+        // Tiene que estar lanzada la base de datos de mongodb del docker-compose. Como está fuera del clúster usa localhost
+        mongoose.connect('mongodb://localhost:27017/vendorvert')
+        .then(() => {
+            logger.info('MongoDB connected...') 
+            console.log('MongoDB connected...') })
+        .catch(err => {
+            logger.error(err)
+            console.log(err)
+        });
+        mongoose.set('strictQuery', false);
 });
+start()
+
+
+
