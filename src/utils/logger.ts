88867a673a0085e917aa071abb4898e20c9ec98a
José Alberto Gómez García <modej@correo.ugr.es>
2023-01-22 12:08:11 +0100

    [#44] Rewrite logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,18 +1,11 @@
-const moment = require("moment");
-import { configuration } from "../config";
-import * as fs from "fs";
-import pino from "pino";
+const pino = require('pino')
+const pinoElastic = require('pino-elasticsearch')
+const streamToElastic = pinoElastic({
+     index: 'vendorvert-logs',
+     consistency: 'one',
+     node: 'http://elasticsearch:9200'
+})
+const logger = pino({level: 'info'}, streamToElastic)
+export {logger}
 
-const log_directory = configuration.log_directory
-const log_file_path = configuration.log_file_path
-const log_file_extension = configuration.log_file_extension
 
-if(!fs.existsSync(log_directory)) {
-     fs.mkdirSync(log_directory,{ recursive: true })
-}
-
-const real_log_file_path = log_file_path+`-${moment().format('YYYY-MM-DD')}`+log_file_extension
-
-export const logger = pino({
-     timestamp: () => `,"time":"${moment().format('HH:mm:ss')}"`,
-}, pino.destination(real_log_file_path))
